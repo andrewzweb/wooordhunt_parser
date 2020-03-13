b47e67207b24f09d3375058a03332bc0d24072d3
andrewzweb <andrewzweb@gmail.com>
2020-03-13 10:24:21 +0200

    write parser page with tests

--- test_parser.py
+++ test_parser.py
@@ -13,12 +13,37 @@ class BaseSettingsTest(unittest.TestCase):
 class GetPageTest(unittest.TestCase):
 
     def test_get_url_exist(self):
-        self.assertTrue(get_page())
+        self.assertFalse(get_page())
 
-    def test_get_page_get_target_page(self):
+    def NOtest_get_page_get_target_page(self):
         resp = get_page(TargetUrl)
         self.assertEqual(resp.status_code, 200)
 
+class ParsePageTest(unittest.TestCase):
+    
+    targetUrl = 'https://wooordhunt.ru/word/fire'
+    response = get_page(targetUrl)
+    parse = Parse(response)
+
+    def test_parse_exist(self):
+        self.assertTrue(Parse(self.response)) 
+
+    def test_parse_title(self):
+        self.assertEqual(self.parse.parse_title(), 'Fire')
+    
+    def test_parse_transcription(self):
+        self.assertEqual(self.parse.parse_transcription(), '|ˈfaɪər|')
+
+    def test_parse_definition(self):
+        result = 'огонь'
+        self.assertIn(result, self.parse.parse_definition_ru())
+
+    
+    def test_parse_phrases(self):
+        result = 'to blanket the fire with sand'
+        self.assertEqual(len(self.parse.parse_phrases()), 10)
+        self.assertIn(result, self.parse.parse_phrases()[1]['eng'])
+        
 
 if __name__ == "__main__":
     unittest.main()
--- wooordhunt_parser.py
+++ wooordhunt_parser.py
@@ -1,4 +1,5 @@
 import requests
+from bs4 import BeautifulSoup as bs 
 
 MainUrl = 'https://wooordhunt.ru/'
 TargetUrl = 'https://wooordhunt.ru/'
@@ -7,5 +8,62 @@ def get_page(url=None):
     if url:
         resp = requests.get(url)
         return resp
-    return True
+    return False
 
+
+class Parse():
+    
+    def __init__(self, raw_page):
+        if raw_page: 
+            self.page = bs(raw_page.text, 'html.parser') 
+        else:
+            pass
+
+    def parse_title(self, ):
+        title = self.page.find('h1').get_text().split('-')[0].strip()
+        return title
+
+
+    def parse_transcription(self, ):
+        transcription = self.page.find_all('span', class_='transcription')[0].get_text().strip()
+        return transcription
+
+    def parse_definition_ru(self, ):
+        definition = self.page.find("span", class_="t_inline_en").get_text().split(',')
+        return definition
+    
+    def parse_phrases(self):
+        phrases = self.page.find("div", class_="phrases").get_text()
+        raw_list_phrases = phrases.split('   ')
+        
+        word_phrases = {}
+        numb = 1
+
+        for item in raw_list_phrases:
+            try:
+                res = item.split('\u2002—\u2002')
+                word_phrases[numb] = { 'eng': res[0], 'rus': res[1],}
+            except: 
+                pass
+
+            numb += 1
+                
+        return word_phrases
+
+
+
+    def parse_phrases_example(self):
+
+        # cleaning data 
+        for item in phrases.split(' '):
+            print(item.split('\u2002—\u2002'))
+
+        #print(phrases)
+
+        examples = soup.find_all('div', class_='block')[2]
+        ru = examples.find_all('div', class_='ex_o')
+        print(ru)
+    
+
+        print(phrases)
+        return phrases
